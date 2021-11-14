579cb3bcb98f71ba930584c2e2a8b1af197f1e53
Tanner Dale <tanner@tannerdale.com>
2021-11-14 17:44:56 -0600

    Add forecast facade, have method return json

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,13 +1,17 @@
 class ForecastFacade
   class << self
     def weather_data(city)
-      OpenWeatherService.weather_data(city_coords(city))
+      ForecastSerializer.serialize(raw_weather_data(city))
     end
 
     private
 
+    def raw_weather_data(city)
+      OpenWeatherService.weather_data(city_coords(city))
+    end
+
     def city_coords(city)
-      MapQuestService.location_lat_lng(city)
+      MapQuestService.location_lat_lon(city)
     end
   end
 end
