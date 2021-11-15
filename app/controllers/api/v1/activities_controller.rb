17a5a5da2ac06fe3cfc65cfc3a07fcc5b223fadd
Tanner Dale <tanner@tannerdale.com>
2021-11-15 12:46:48 -0600

    Add params presence validation

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ActivitiesController < ApplicationController
+  before_action { present?(:destination) }
+
   def show
     render json: ActivityFacade.activities(params[:destination])
   end
