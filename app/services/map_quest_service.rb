225f230bff0beeb067281efcdec4fe1ae2e5af79
Tanner Dale <tanner@tannerdale.com>
2021-11-16 12:48:55 -0600

    Cache route info based on to/from cities

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -7,7 +7,9 @@ class MapQuestService
     end
 
     def road_trip(to, from)
-      route = MapQuestClient.fetch(road_trip_url(to, from))
+      route = Rails.cache.fetch "#{to}, #{from}" do
+        MapQuestClient.fetch(road_trip_url(to, from))
+      end
 
       raise ImpossibleRoute if route[:info][:statuscode] == 402
 
