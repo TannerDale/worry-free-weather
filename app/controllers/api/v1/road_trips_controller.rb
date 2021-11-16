bcffefcf9b6c0218beacb8f254dd9c41223516a7
Tanner Dale <tanner@tannerdale.com>
2021-11-16 11:29:45 -0600

    Add api key, param validations

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::RoadTripsController < ApplicationController
+  include ApiKeyValidator
+
+  before_action { all_present?(%i[destination origin]) }
+  before_action :validate_api_key
+
   def create
     trip = RoadTripFacade.road_trip(params[:destination], params[:origin])
 
