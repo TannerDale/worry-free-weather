91988f950bff69965c52ac0611d5e33df4b748ba
Tanner Dale <tanner@tannerdale.com>
2021-11-16 11:30:23 -0600

    Add sad path testing for road trips

--- spec/requests/v1/road_trip_requests_spec.rb
+++ spec/requests/v1/road_trip_requests_spec.rb
@@ -8,9 +8,9 @@ describe 'Road Trip Requests', :vcr do
   let(:attributes) { data[:attributes] }
 
   describe 'POST /v1/road_trips' do
-    context 'with valid params' do
-      let(:params) { { origin: 'austin,tx', destination: 'denver,co', api_key: user.api_key } }
+    let(:params) { { origin: 'austin,tx', destination: 'denver,co', api_key: user.api_key } }
 
+    context 'with valid params' do
       before { post api_v1_road_trip_path, headers: headers, params: params.to_json }
 
       it 'returns status code 200' do
@@ -30,6 +30,43 @@ describe 'Road Trip Requests', :vcr do
 
         expect(attributes[:weather_at_eta]).to be_a Hash
         expect(attributes[:weather_at_eta].size).to eq 2
+      end
+    end
+
+    describe 'with invalid params' do
+      it 'rejects no origin city' do
+        post api_v1_road_trip_path, headers: headers, params: params.merge({ origin: '' }).to_json
+
+        expect(response).to have_http_status 400
+        expect(parsed[:error][:details]).to eq('Missing origin field(s)')
+      end
+
+      it 'rejects no destination city' do
+        post api_v1_road_trip_path, headers: headers, params: params.merge({ destination: '' }).to_json
+
+        expect(response).to have_http_status 400
+        expect(parsed[:error][:details]).to eq('Missing destination field(s)')
+      end
+
+      it 'has both missing params when no destination and origin' do
+        post api_v1_road_trip_path, headers: headers, params: params.merge({ destination: '', origin: '' }).to_json
+
+        expect(response).to have_http_status 400
+        expect(parsed[:error][:details]).to eq('Missing destination and origin field(s)')
+      end
+
+      it 'rejects no api key' do
+        post api_v1_road_trip_path, headers: headers, params: params.merge({ api_key: '' }).to_json
+
+        expect(response).to have_http_status 401
+        expect(parsed[:error][:details]).to eq('No api key provided')
+      end
+
+      it 'rejects incorrect api key' do
+        post api_v1_road_trip_path, headers: headers, params: params.merge({ api_key: 'asdfasd' }).to_json
+
+        expect(response).to have_http_status 401
+        expect(parsed[:error][:details]).to eq('Invalid api key')
       end
     end
   end
